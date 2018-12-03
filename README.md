b60dece9e1583ec170519e4e8473349a89cc5c1d
Nathan Liboutet <nathan.lib19@gmail.com>
2018-12-03 14:31:50 +0100

    ajout documentation

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# JavaScript
+<h1> JavaScript - WebSocket <h1>
+
+<p> Ce projet permet de créer un WebSocket qui permet l'échange de message 
+entre un client et un serveur à travers un page html </p>
--- client/script.js
+++ client/script.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+    // On initialise notre variable "ws" avec un WebSocket où on indique l'url
+    // à laquelle le client se connecte et le serveur répond
     var ws = new WebSocket('ws://127.0.0.1:8080/chat?pseudo=toto');
     console.log(ws);
 
+    // A chaque appel de la page, le client appelle la fonction qui envoie
+    // une donnée au serveur, ici le message "Texte"
     ws.onopen = function() {
         ws.send("Texte");
     };
